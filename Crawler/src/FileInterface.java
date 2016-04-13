dfa96bf1d3f776b807d45389a5424c16678e6b2e
YipManDan <Yeh.DanielP@gmail.com>
2016-04-13 15:35:31 -0700

    Created a File Chooser Interface

---
+++ Crawler/src/FileInterface.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Daniel on 4/6/2016.
+ */
+public class FileInterface extends JFrame implements ActionListener{
+
+    private final JFileChooser fc = new JFileChooser();
+
+    private JTextField tf;
+    private JButton open, ok;
+
+    FileInterface(){
+
+        super("Select a Specification File");
+
+        JPanel north, south;
+
+        //North panel contains text field displaying selected file path
+        north = new JPanel(new GridLayout(1,1));
+        tf = new JTextField();
+        tf.setText("Select a file: ");
+        tf.setEditable(false);
+        tf.setBackground(Color.white);
+        north .add(tf);
+
+        //South panel contains select and confirm button
+        south = new JPanel();
+        open = new JButton("Select a File");
+        open.addActionListener(this);
+        ok = new JButton("OK");
+        ok.addActionListener(this);
+        south.add(open);
+        south.add(ok);
+
+        add(north, BorderLayout.NORTH);
+        add(south, BorderLayout.CENTER);
+
+        this.setPreferredSize(new Dimension(400, 140));
+        this.setMinimumSize(new Dimension(400, 140));
+        this.setLocationRelativeTo(null);
+
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object o = e.getSource();
+        if(o == open) {
+            int returnVal = fc.showOpenDialog(this);
+            if(returnVal == JFileChooser.CANCEL_OPTION)
+                return;
+            tf.setText(fc.getSelectedFile().getAbsolutePath());
+            return;
+        }
+        if(o == ok) {
+            //();
+            this.dispose();
+            return;
+        }
+
+    }
+
+    public static void main(String[] args){
+        new FileInterface();
+    }
+
+}
--- Crawler/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Daniel on 4/6/2016.
- */
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-
-}
