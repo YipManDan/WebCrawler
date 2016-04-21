fd2286bf31c56d76d359da9ab053320504dcc3de
JesseHarder <jhharder@scu.edu>
2016-04-20 20:20:37 -0700

    Changed domain restricion check to use startsWith() instead of contains().

--- Crawler/src/Crawler.java
+++ Crawler/src/Crawler.java
@@ -72,7 +72,7 @@ public class Crawler {
                         continue;
                     }
                 }
-                else if (URLRestriction() != null && !urlToCrawl.toString().contains(URLRestriction().toString())) {
+                else if (URLRestriction() != null && !urlToCrawl.toString().startsWith(URLRestriction().toString())) {
                     // If the URL restriction is not within the url we are about to crawl, do not crawl it.
                     // Could do this: URLs_not_to_crawl.add(urlToCrawl.toString());
                     continue;
