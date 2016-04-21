283f3b669ba2ff9871bee35c352da35de597a8ec
JesseHarder <jhharder@scu.edu>
2016-04-20 20:21:46 -0700

    Started working on NoiseReducer's constructor. Did not get far.

--- NoiseCanceler/src/NoiseReducer.java
+++ NoiseCanceler/src/NoiseReducer.java
@@ -2,4 +2,11 @@
  * Created by Daniel on 4/20/2016.
  */
 public class NoiseReducer {
+    private String inputPath;
+    private String outputPath;
+
+    NoiseReducer() {
+        System.err.println("Input and output paths hardcoded to Jesse's computer currently.");
+//        inputhPath = ""
+    }
 }
