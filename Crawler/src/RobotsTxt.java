e1770c574fa225bfb83a6d9e3c77923787504d93
YipManDan <Yeh.DanielP@gmail.com>
2016-04-22 13:40:45 -0700

    Created RobotsTxt.java Brainstorm ideas

---
+++ Crawler/src/RobotsTxt.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Daniel on 4/22/2016.
+ */
+public class RobotsTxt {
+    //given a url, class will download robots.txt
+    //Will parse robots.txt and add to dont access list? or return true/false, can access?
+    //Maybe it will be in between spider and adding to queue?
+}
