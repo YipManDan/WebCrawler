7b0888e9ccf8b4739fb124adf75fcc2aeccbe953
YipManDan <Yeh.DanielP@gmail.com>
2016-04-21 21:59:37 -0700

    Modified ContentFinder.java  Wrote findContent. (Untested)

--- NoiseReducer/src/ContentFinder.java
+++ NoiseReducer/src/ContentFinder.java
@@ -22,20 +22,43 @@ public class ContentFinder {
         //sums
         int low, mid, high;
         int sum, bestSum;
-        int midNext = 0, highNext = 0;
+        int midNext, highNext;
+        /*
+        integer i will point to the upper inclusive bound of low.
+        integer j will point to the lower inclusive bound of high.
+        (mid) will be between i and j, exclusive.
+         */
 
-        //Get initial values for low, mid, and high
-        low = parseTuples.get(lowPosition).getBit();
-        mid = parseTuples.get(lowPosition + 1).getBit();
-        high = 0;
-        for(i = lowPosition + 2; i < parseTuples.size(); i++){
-            high += parseTuples.get(i).getBit();
+        low = 0;
+        sum = 0;
+        bestSum = sum;
+        //Initialize midNext and highNext which will be the initial values for mid and high
+        midNext = (1 - parseTuples.get(1).getBit());
+        highNext = 0;
+        for(j = 2; j < parseTuples.size(); j++){
+            highNext += parseTuples.get(j).getBit();
         }
 
-        for(i = lowPosition; i < parseTuples.size()-1; i++){
-            j = i + 1;
-            midNext = midNext + (1 - parseTuples.get(i + 1).getBit());
-            highNext = highNext - parseTuples.get(j).getBit();
+        for(i = 0; i < parseTuples.size()-1; i++){
+            low += parseTuples.get(i).getBit();
+            mid = midNext;
+            high = highNext;
+            midNext += (1 - parseTuples.get(i + 1).getBit());
+            highNext -= parseTuples.get(i + 2).getBit();
+
+            //initially low starts at 0 then.
+            //initially high starts where? I think at i + 1?
+            for(j = i + 2; j < parseTuples.size(); j++){
+                mid += (1 - parseTuples.get(j - 1).getBit());
+                high -= parseTuples.get(j).getBit();
+                sum = low + mid + high;
+                if(sum > bestSum){
+                    bestSum = sum;
+                    lowPosition = parseTuples.get(i).getPosition();
+                    highPosition = parseTuples.get(j).getPosition();
+                }
+
+            }
         }
 
 
