589ded69a1facfab726301e63452d611ba525a58
YipManDan <Yeh.DanielP@gmail.com>
2016-04-27 10:45:45 -0700

    RobotsChecker.java
    Code cleanup and commenting.

--- Crawler/src/RobotsChecker.java
+++ Crawler/src/RobotsChecker.java
@@ -7,29 +7,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Daniel on 4/22/2016.
+ * Class to handle robots.txt file from webhosts.
+ * The class will download the robots.txt file in order to determine
+ *  which pages a spider shall be allowed on as well as the crawl-delay.
+ *
+ * @Author Daniel Yeh and Jesse Harder
  */
 public class RobotsChecker {
 
+    //Data
     private URL url;
-    private InputStream inputStream;
-    private BufferedReader bufferedReader;
     private String line, userAgent;
     private String[] parsedLine;
 
+    // Data which the parent class to can use to determine allowed status and crawl-delay
     protected List<URL> disallowedURLs;
-    protected int crawlDelay;
+    protected int crawlDelay = -1;
 
+    // For downloading the robots.txt file
+    private InputStream inputStream;
+    private BufferedReader bufferedReader;
+
+    /**
+     * Constructor of RobotsChecker
+     * @param url   The url the crawler wants to crawl
+     */
     RobotsChecker(URL url){
+
+        // Store a local copy of the url
         try {
             this.url = new URL(url.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        crawlDelay = -1;
+
         disallowedURLs = new ArrayList<URL>();
+
+        // Generate the URL of the robots.txt file
         String robotURL = "http://" + url.getHost() + "/robots.txt";
 
+        // Create a bufferedReader to download the robots.txt file
         try{
             System.out.println("Accessing: " + robotURL);
             inputStream = (new URL(robotURL)).openStream();
@@ -40,42 +57,65 @@ public class RobotsChecker {
             return;
         }
 
+        //Call method to download and store relevant information
         getDisallowList();
     }
 
+    /**
+     * Method will download the robots.txt file line-by-line and skip
+     * to the relevant sections directed towards our crawler.
+     * Disallowed links will be stored and if a crawl-delay is specified it will be saved.
+     */
     protected void getDisallowList(){
+
         String disallowedURL;
         userAgent = "";
+
         try {
+            // Loop until the end of the robots.txt file
             while ((line = bufferedReader.readLine()) != null) {
 //                System.out.println(line);
+
+                //If the line is empty, then the file is switching sections. Reset the userAgent variable
                 if (line.length() == 0)
                     userAgent = "";
+
                 else if (line.contains("User-agent:")) {
+                    // Parse and store the specified user-agent
                     parsedLine = line.split(" ");
                     userAgent = parsedLine[1];
 //                    System.out.println("User-agent is: " + userAgent);
                 }
+
+                // Check if the user-agent specified in this block of text is specific to our crawler
                 else if (userAgent.equals("*")) {
+                    // Parses the line
                     parsedLine = line.split(" ");
                     String lineStart = parsedLine[0];
+
+                    // If the line indicates the crawl-delay, store it
                     if (lineStart.toLowerCase().startsWith("crawl-delay"))
                         crawlDelay = Integer.parseInt(parsedLine[1]);
+
+                    // If the line lists a URL path to disallow, then add to disallow list
                     else if (parsedLine[0].toLowerCase().startsWith("disallow")) {
                         disallowedURL = prepareURL(parsedLine[1]);
-//                        System.out.println("Disallowed: " + disallowedURL);
-                            disallowedURLs.add(new URL("http://" + url.getHost() + disallowedURL));
+//                      System.out.println("Disallowed: " + disallowedURL);
+                        disallowedURLs.add(new URL("http://" + url.getHost() + disallowedURL));
+
                     }
                 }
             }
 
+            //Close the bufferedReader and inputStream
             bufferedReader.close();
+            inputStream.close();
         }
         catch (Exception e){
             System.err.println("Exception at getDisallowList: ");
             e.printStackTrace();
         }
-
+//        Output list of disallowed URLs
 //        System.out.printf("Disallowed List:");
 //        for (URL url: disallowedURLs) {
 //            System.out.println(url);
@@ -84,14 +124,30 @@ public class RobotsChecker {
 //        System.out.println("");
     }
 
+    /**
+     * Public method to determine if input URL is allowed by the host.
+     * @param url   URL the Crawler wants to access
+     * @return  Return true if the Crawler is allowed to crawl the page, else false.
+     */
     public boolean isAllowed(URL url){
+
+        //Loop through all URL's in the list
         for(URL checkURL : disallowedURLs){
+
+            //Use regex matching to determine if the URL is disallowed
             if(url.toString().matches(checkURL.toString()))
                 return false;
         }
+
         return true;
+
     }
 
+    /**
+     * Method to change URL to allow for regex matching
+     * @param disallowedURL URL to be added to disallowList
+     * @return  Returns a String which a modified URL allowing for regex matching.
+     */
     private String prepareURL(String disallowedURL) {
         disallowedURL = disallowedURL.replace(".", "\\.");
         disallowedURL = disallowedURL.replace("*",".*");
